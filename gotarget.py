49f9ba48c94d501618d4777552f8010d43a59758
XiaoxingChen <cxx@seer-robotics.com>
2017-11-17 13:14:49 +0800

    Modify: add go target

---
+++ gotarget.py
@@ -0,0 +1,42 @@
+from rbkNetProtoEnums import *
+import rbkNetProtoEnums
+import json
+import socket
+import os
+import time
+
+so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+so.connect(('192.168.192.5', API_PORT_TASK ))
+# so.connect(('127.0.0.1', API_PORT_STATE))
+so.settimeout(5)
+
+
+so.send(packMsg(1, robot_task_gotarget_req ,
+                {"id": "LM1" }))
+try:
+    data = so.recv(16)
+except socket.timeout:
+    print('timeout')
+    quit()
+
+jsonDataLen = 0
+backReqNum = 0
+if(len(data) < 16):
+    print('pack head error')
+    print(data)
+    os.system('pause')
+    so.close()
+    quit()
+else:
+    jsonDataLen, backReqNum = unpackHead(data)
+    print('json datalen: %d, backReqNum: %d' % (jsonDataLen, backReqNum))
+
+if(jsonDataLen > 0):
+    data = so.recv(1024)
+    ret = json.loads(data)
+
+    print(ret)
+
+# os.system('pause')
+time.sleep(1)
+so.close()
